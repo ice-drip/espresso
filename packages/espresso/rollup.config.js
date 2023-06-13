b3e3346d1dac5551453d0ea1c461c7d23fc3f726
rikka <admin@rikka.cc>
2023-06-13 23:15:22 +0800

    init

--- packages/espresso/rollup.config.js
+++ packages/espresso/rollup.config.js
@@ -1,7 +1,7 @@
 import typescript from "rollup-plugin-typescript2";
 import dts from "rollup-plugin-dts";
-import {resolve} from "path";
-import {rmdirSync} from "fs";
+import { resolve } from "path";
+import { existsSync, rmdirSync } from "fs";
 import { execSync } from "child_process";
 const config = [
   {
@@ -9,45 +9,46 @@ const config = [
     plugins: [
       buildUtil(),
       typescript({
-        tsconfigOverride: { compilerOptions: { module: "es2015" } }
-      })
+        tsconfigOverride: { compilerOptions: { module: "es2015" } },
+      }),
     ],
     output: [
       {
         format: "cjs",
         sourcemap: true,
         preserveModules: true,
-        dir: "dist/cjs"
+        dir: "dist/cjs",
       },
       {
         format: "esm",
         sourcemap: true,
         preserveModules: true,
-        dir: "dist/esm"
+        dir: "dist/esm",
       },
       {
         format: "umd",
         name: "espresso",
         sourcemap: true,
-        file: "dist/umd/index.umd.js"
-      }
-    ]
+        file: "dist/umd/index.umd.js",
+      },
+    ],
   },
   {
     input: "src/index.ts",
     plugins: [dts()],
-    output: [{ file: "dist/typings/index.d.ts", format: "es" }]
-  }
+    output: [{ file: "dist/typings/index.d.ts", format: "es" }],
+  },
 ];
 function buildUtil() {
   return {
     name: "build-util",
     buildStart() {
-      rmdirSync(resolve(process.cwd(),"dist"),{recursive:true});
+      if (existsSync(resolve(process.cwd(), "dist")))
+        rmdirSync(resolve(process.cwd(), "dist"), { recursive: true });
     },
-    closeBundle(){
-      execSync("tsc --project tsconfig.type.json")
-    }
+    closeBundle() {
+      execSync("tsc --project tsconfig.type.json");
+    },
   };
 }
 export default config;
