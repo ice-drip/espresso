0bdf839f31f9f358f7f7a0a25bb5a884b1e02125
Rikka <admin@rikka.cc>
2026-06-23 13:31:46 +0800

    test: add encoding tests for hex, base64, utf8, latin1, and utf16

---
+++ packages/test/src/enc/utf16.test.ts
@@ -0,0 +1,13 @@
+import { utf16Encode, utf16Decode } from '@kaffee/espresso';
+
+describe('UTF-16 encoding', () => {
+  it('decodes UTF-16BE bytes', () => {
+    const bytes = utf16Decode('A', false);
+    expect(bytes).toEqual(new Uint8Array([0x00, 0x41]));
+  });
+
+  it('decodes UTF-16LE bytes', () => {
+    const bytes = utf16Decode('A', true);
+    expect(bytes).toEqual(new Uint8Array([0x41, 0x00]));
+  });
+});
