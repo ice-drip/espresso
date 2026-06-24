8167ca271bfa51972705f77960c0f94afcdb0e7f
Rikka <admin@rikka.cc>
2026-06-24 21:21:03 +0800

    ci: clean up release workflow for Trusted Publisher, use CLI args for bumpp

--- bumpp.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "bumpp";
-
-export default defineConfig({
-  tag: "@kaffee/espresso@${version}",
-  push: true,
-  commit: "chore(release): @kaffee/espresso@${version}",
-  files: ["packages/espresso/package.json"],
-  execute: "pnpm run release:check && pnpm run release:changelog",
-});
